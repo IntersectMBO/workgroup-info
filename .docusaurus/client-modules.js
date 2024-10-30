8a4a378db3550ffc25085533f119ac6aa7172b1e
Carlos LopezDeLara <carlos.lopezdelara@iohk.io>
2024-10-30 11:54:49 -0600

    this will change the default font size on index.mdx

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -2,4 +2,5 @@ export default [
   require("/home/carlos/github/workgroup-info/node_modules/infima/dist/css/default/default.css"),
   require("/home/carlos/github/workgroup-info/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/home/carlos/github/workgroup-info/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/carlos/github/workgroup-info/src/css/custom.css"),
 ];
